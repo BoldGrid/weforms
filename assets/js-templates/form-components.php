fcb8e01e5ef00f083bfd8b24040dcf98f943e5c2
Tareq Hasan <tareq1988@gmail.com>
2017-07-25 11:52:06 +0600

    Complete the slack integration

--- assets/js-templates/form-components.php
+++ assets/js-templates/form-components.php
@@ -98,23 +98,11 @@
 <div>
     <div class="wpuf-int-form-row">
         <div class="wpuf-int-field-label">
-            <label for="mailchimp-list-id"><?php _e( 'Message', 'weforms' ); ?></label>
-        </div>
-        <div class="wpuf-int-field">
-            <div class="wpuf-int-field-small">
-                <textarea class="regular-text" v-model="settings.message" id="" cols="30" rows="10"></textarea>
-                <wpuf-merge-tags v-on:insert="insertValue" field="message"></wpuf-merge-tags>
-            </div>
-        </div>
-    </div>
-
-    <div class="wpuf-int-form-row">
-        <div class="wpuf-int-field-label">
             <label for="mailchimp-list-id"><?php _e( 'Slack Webhook URL', 'weforms' ); ?></label>
         </div>
         <div class="wpuf-int-field">
             <input type="url" class="regular-text" v-model="settings.url" placeholder="https://hooks.slack.com/services/...">
-            <p class="help"><?php _e( 'Slack webhook URL to send our JSON payloads', 'textdomain' ); ?></p>
+            <p class="help"><?php _e( 'Slack webhook URL to send our JSON payloads', 'weforms' ); ?></p>
         </div>
     </div>
 </div></script>
--- includes/integrations/slack/class-integration-slack.php
+++ includes/integrations/slack/class-integration-slack.php
@@ -17,11 +17,93 @@ class WeForms_Integration_Slack extends WPUF_Abstract_Integration {
 
         $this->settings_fields = array(
             'enabled' => false,
-            'message' => '',
             'url'     => '',
         );
 
         add_filter( 'weforms_form_builder_integrations', array( $this, 'register_integration_settings' ) );
+        add_action( 'weforms_entry_submission', array( $this, 'send_notification' ), 10, 4 );
+    }
+
+    /**
+     * Subscribe a user when a form is submitted
+     *
+     * @param  int $entry_id
+     * @param  int $form_id
+     * @param  int $page_id
+     * @param  array $form_settings
+     *
+     * @return void
+     */
+    public function send_notification( $entry_id, $form_id, $page_id, $form_settings ) {
+
+        $integration = wpuf_is_integration_active( $form_id, $this->id );
+
+        if ( false === $integration ) {
+            return;
+        }
+
+        if ( empty( $integration->url ) ) {
+            return;
+        }
+
+        $attachment_fields = array();
+        $title             = sprintf( __( "#%d New submission on %s", 'weforms' ), $entry_id, get_post_field( 'post_title', $form_id ) );
+        $form_data         = weforms_get_entry_data( $entry_id );
+
+        if ( false === $form_data ) {
+            return;
+        }
+
+        // prepare the attachment data
+        foreach ( $form_data['fields'] as $meta_key => $value ) {
+            $is_short = true;
+            $_value   = $form_data['data'][ $meta_key ];
+
+            if ( 'textarea' == $value['type'] ) {
+                $is_short = false;
+                $_value   = html_entity_decode( $_value );
+
+                // replace paragrphs and breaks
+                $_value = str_replace( '<p>', '', $_value );
+                $_value = str_replace( '<br />', '', $_value );
+                $_value = str_replace( '</p>', "\n", $_value );
+            }
+
+            $attachment_fields[] = array(
+                'title' => $value['label'],
+                'value' => $_value,
+                'short' => $is_short
+            );
+        }
+
+        $data = array(
+            'payload' => json_encode( array(
+            'username'    => 'weForms',
+            'icon_url'    => '',
+            'attachments' => array(
+                array(
+                    'fallback'    => $title,
+                    'color'       => '#36a64f',
+                    'title'       => $title,
+                    'title_link'  => sprintf( '%s#/form/%d/entries/%d', admin_url( 'admin.php?page=weforms' ), $form_id, $entry_id ),
+                    'fields'      => $attachment_fields,
+                    'footer'      => 'weForms',
+                    'ts'          => current_time( 'timestamp' )
+                )
+            )
+        ) ) );
+
+        $posting_to_slack = wp_remote_post( $integration->url, array(
+            'method'      => 'POST',
+            'timeout'     => 30,
+            'redirection' => 5,
+            'httpversion' => '1.0',
+            'blocking'    => false,
+            'headers'     => array(),
+            'body'        => $data,
+            'cookies'     => array()
+        ) );
+
     }
 
 }
