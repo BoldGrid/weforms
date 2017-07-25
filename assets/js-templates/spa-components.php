dfa7e5b8153fca1341a87c093ed934f0cfea28e1
Tareq Hasan <tareq1988@gmail.com>
2017-07-25 13:42:27 +0600

    premium upgrade page

--- assets/js-templates/spa-components.php
+++ assets/js-templates/spa-components.php
@@ -569,10 +569,74 @@
 </div></script>
 
 <script type="text/x-template" id="tmpl-wpuf-weforms-premium">
-<div class="wrap bcf-extension-list">
-    <h2><?php _e( 'weForms Premium', 'weforms' ); ?></h2>
+<div class="wrap weforms-premium about-wrap">
+    <h1><?php _e( 'weForms Pro', 'weforms' ); ?></h1>
 
-    <p>Upgrade to weForms premium to unlock features</p>
+    <p class="about-text"><?php _e( 'Upgrade to weForms premium to unlock even more features.', 'weforms' ); ?></p>
+    <div class="wp-badge">weForms Pro</div>
+
+    <hr>
+    <div class="feature-section one-col">
+        <div class="col">
+            <h2><?php _e( 'More Features', 'weforms' ); ?></h2>
+            <p>weForms Pro is designed to be used with your business need. Every package is designed and curated by keeping your requirements in mind.</p>
+        </div>
+    </div>
+
+    <div class="feature-section two-col">
+        <div class="col">
+            <h3>Image Widget</h3>
+            <p>Adding an image to a widget is now a simple task that is achievable for any WordPress user without needing to know code. Simply insert your image right within the widget settings. Try adding something like a headshot or a photo of your latest weekend adventure — and see it appear automatically.</p>
+        </div>
+
+        <div class="col">
+            <h3>Video Widget</h3>
+            <p>A welcome video is a great way to humanize the branding of your website. You can now add any video from the Media Library to a sidebar on your site with the new Video widget. Use this to showcase a welcome video to introduce visitors to your site or promote your latest and greatest content.</p>
+        </div>
+
+        <div class="col">
+            <h3>Rich Text Widget</h3>
+            <p>This feature deserves a parade down the center of town! Rich-text editing capabilities are now native for Text widgets. Add a widget anywhere and format away. Create lists, add emphasis, and quickly and easily insert links. Have fun with your newfound formatting powers, and watch what you can accomplish in a short amount of time.</p>
+        </div>
+
+        <div class="col">
+            <h3>Audio Widget</h3>
+            <p>Are you a podcaster, musician, or avid blogger? Adding a widget with your audio file has never been easier. Upload your audio file to the Media Library, go to the widget settings, select your file, and you’re ready for listeners. This would be a easy way to add a more personal welcome message, too!</p>
+        </div>
+    </div>
+
+    <hr>
+
+    <h2><?php _e( 'More Integrations', 'weforms' ); ?></h2>
+
+    <div class="headline-feature four-col">
+        <div class="col">
+            <img src="http://via.placeholder.com/240x160&text=MailChimp" alt="">
+            <h3>MailChimp</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In officiis repellat ea molestias.</p>
+        </div>
+        <div class="col">
+            <img src="http://via.placeholder.com/240x160&text=Campaign+Monitor" alt="">
+            <h3>Campaign Monitor</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In officiis repellat ea molestias.</p>
+        </div>
+        <div class="col">
+            <img src="http://via.placeholder.com/240x160&text=Constant+Contact" alt="">
+            <h3>Constant Contact</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In officiis repellat ea molestias.</p>
+        </div>
+        <div class="col">
+            <img src="http://via.placeholder.com/240x160&text=MailPoet" alt="">
+            <h3>MailPoet</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In officiis repellat ea molestias.</p>
+        </div>
+    </div>
+
+    <div style="display: block; height: 100px; overflow: hidden;"></div>
+
+    <div class="weforms-upgrade-sticky-footer">
+        <p><a class="button button-primary" href="https://wedevs.com/weforms-upgrade/" target="_blank"><?php _e( 'Upgrade Now', 'weforms' ); ?></a></p>
+    </div>
 
 </div></script>
 
