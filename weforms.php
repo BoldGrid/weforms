d25cf5abe0a978ea8d8f509eda86abb17eec5432
Edi Amin <to.ediamin@gmail.com>
2017-08-28 08:41:25 +0600

    Add WPForms importer (#25)
    * Add WPForms importer
    
    * Fix options key

--- weforms.php
+++ weforms.php
@@ -201,6 +201,7 @@ final class WeForms {
         if ( is_admin() ) {
             require_once WEFORMS_INCLUDES . '/admin/class-admin.php';
             require_once WEFORMS_INCLUDES . '/importer/class-importer-cf7.php';
+            require_once WEFORMS_INCLUDES . '/importer/class-importer-wpforms.php';
             require_once WEFORMS_INCLUDES . '/admin/class-form-template.php';
             require_once WEFORMS_INCLUDES . '/admin/class-pro-integrations.php';
         } else {
@@ -354,6 +355,7 @@ final class WeForms {
             new WeForms_Form_Template();
             new WeForms_Pro_Integrations();
             new WeForms_Importer_CF7();
+            new WeForms_Importer_WPForms();
         } else {
             new WeForms_Frontend();
         }
