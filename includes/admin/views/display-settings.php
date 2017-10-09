d878226b660f4235368ca7caf5a4229b4baad233
Mohaiminul Islam <sk.shaikat18@gmail.com>
2017-10-09 13:07:04 +0600

    done #53 Popup form template that could work like optin

--- includes/admin/views/display-settings.php
+++ includes/admin/views/display-settings.php
@@ -23,4 +23,46 @@
             </p>
         </td>
     </tr>
+    <tr class="wpuf-limit-entries">
+        <th><?php _e( 'Modal Form', 'weforms' ); ?></th>
+        <td>
+            <label>
+                <input type="checkbox" v-model="settings.modal_form" :true-value="'true'" :false-value="'false'">
+                <?php _e( 'Enable modal form', 'weforms' ); ?>
+            </label>
+
+            <p class="description">
+                <?php _e( 'The form will apper in modal.', 'weforms' ) ?>
+            </p>
+        </td>
+    </tr>
+
+    <tr class="wpuf-appearance" v-show="settings.modal_form == 'true'">
+        <th><?php _e( 'appearance', 'weforms' ); ?></th>
+        <td>
+            <select v-model="settings.appearance">
+                <?php
+                $appearance_options = array(
+                    'button' => __( 'Button', 'weforms' ),
+                    'link' => __( 'Link', 'weforms' )
+                );
+
+                foreach ($appearance_options as $to => $label) {
+                    printf('<option value="%s"%s>%s</option>', $to, '', $label );
+                }
+                ?>
+            </select>
+            <p class="description">
+                <?php _e( 'The modal form will display upon click on this button / link', 'weforms' ) ?>
+            </p>
+        </td>
+    </tr>
+
+    <tr class="wpuf-same-page" v-show="settings.modal_form == 'true'">
+        <th><?php _e( 'Button / Link text', 'weforms' ); ?></th>
+        <td>
+            <input type="text" value="" v-model="settings.modal_text">
+        </td>
+    </tr>
+
 </table>
--- includes/class-frontend-form.php
+++ includes/class-frontend-form.php
@@ -64,6 +64,23 @@ class WeForms_Frontend_Form {
         $form_fields   = $form->get_fields();
         $form_settings = $form->get_settings();
         $show_credit   = weforms_get_settings( 'credit', false );
+
+        if ( $form_settings['modal_form'] ) {
+            /**
+             * Enqueue scripts
+             *
+             * @param string $handle Script name
+             * @param string $src Script url
+             * @param array $deps (optional) Array of script names on which this script depends
+             * @param string|bool $ver (optional) Script version (used for cache busting), set to null to disable
+             * @param bool $in_footer (optional) Whether to enqueue the script before </head> or before </body>
+             */
+            wp_enqueue_script( 'weforms-modal-js', WEFORMS_ASSET_URI . '/modal/jquery.modal.js', array( 'jquery', 'wpuf-form' ), false, false );
+            wp_enqueue_style( 'weforms_modal_styles', WEFORMS_ASSET_URI . '/modal/jquery.modal.css' );
+            
+            // add_filter( 'weforms_frontend_scripts', array( $this, 'weforms_modal_frontend_script' ) );
+            // add_filter( 'weforms_admin_styles', array( $this, 'weforms_modal_frontend_style' ) );
+        }
         ?>
 
         <script type="text/javascript">
@@ -80,7 +97,7 @@ class WeForms_Frontend_Form {
             }
         </script>
 
-        <form class="wpuf-form-add" action="" method="post">
+        <form class="wpuf-form-add <?php echo $form_settings['modal_form'] ? 'modal' : ''; ?>" action="" method="post" id="<?php echo $form_settings['modal_form'] ? 'modal-form' : ''; ?>" <?php echo $form_settings['modal_form'] ? 'style="display:none"' : ''; ?>>
 
             <ul class="wpuf-form form-label-<?php echo $form_settings['label_position']; ?>">
 
@@ -103,6 +120,14 @@ class WeForms_Frontend_Form {
         </form>
 
         <?php
+        if ( $form_settings['modal_form'] ) {
+            if ( 'link' == $form_settings['appearance'] ) {
+                printf('<p><a href="#modal-form" rel="modal:open">%s</a></p>', $form_settings['modal_text'] );
+            } else if ( 'button' == $form_settings['appearance'] ) {
+                printf('<p><button><a href="#modal-form" rel="modal:open">%s</a></button></p>', $form_settings['modal_text'] );
+            }
+        }
+
         if ( $show_credit ) {
             printf( '<em>' . __( 'Powered by <a href="%s" target="_blank">weForms</a>', 'weforms' ) . '</em>', 'https://wordpress.org/plugins/weforms/' );
         }
@@ -134,5 +159,35 @@ class WeForms_Frontend_Form {
             <input type="submit" class="weforms_submit_btn" name="submit" value="<?php echo $form_settings['submit_text']; ?>" />
         </li>
     <?php
+    }
+
+    /**
+     * Get all modal scripts
+     * 
+     * @param  array   $scripts
+     *
+     * @return array
+     */
+    public function weforms_modal_frontend_script( $scripts ) {
+        $scripts['weforms-modal-js'] = array(
+            'src'       => WEFORMS_ASSET_URI . '/modal/jquery.modal.js',
+            'deps'      => array( 'wpuf-form', 'jquery' ),
+            'in_footer' => false
+        );
+        return $scripts;
+    }
+
+    /**
+     * Get all modal style
+     * 
+     * @param  array   $style
+     *
+     * @return array
+     */
+    public function weforms_modal_frontend_style( $style ) {
+        $style['weforms-modal-css'] = array(
+            'src'  => WEFORMS_ASSET_URI . '/modal/jquery.modal.css',
+        );
+        return $style;
     }
 }
