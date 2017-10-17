1973722ec38f3ef3253085ad57b7c9625abd2ed5
Mohaiminul Islam <sk.shaikat18@gmail.com>
2017-10-17 14:31:45 +0600

    Add Slack help Docs Links in the UI fix #56

--- assets/js-templates/form-components.php
+++ assets/js-templates/form-components.php
@@ -384,7 +384,7 @@
         </div>
         <div class="wpuf-int-field">
             <input type="url" class="regular-text" v-model="settings.url" placeholder="https://hooks.slack.com/services/...">
-            <p class="help"><?php _e( 'Slack webhook URL to send our JSON payloads', 'weforms' ); ?></p>
+            <p class="help"><?php printf( __( '%sSlack webhook URL%s to send our JSON payloads (%sView documentation%s)', 'weforms' ), '<a href="https://api.slack.com/incoming-webhooks" target="_blank" >', '</a>', '<a href="https://wedevs.com/docs/weforms/integrations/slack/" target="_blank" >', '</a>' ); ?></p>
         </div>
     </div>
 </div></script>
