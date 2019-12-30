dcc5ab9cc3ad3588c37a4a413dac1e8a8b4e646c
Rokibul Islam <rrcucse.11@gmail.com>
2019-12-31 00:28:18 +0600

    Fix: hidden field issue

--- includes/fields/class-field-hidden.php
+++ includes/fields/class-field-hidden.php
@@ -20,7 +20,13 @@ class WeForms_Form_Field_Hidden extends WeForms_Field_Contract {
      * @return void
      */
     public function render( $field_settings, $form_id ) {
-        $value = isset($_GET[$field_settings['dynamic']['param_name']]) ? sanitize_text_field(  wp_unslash( $_GET[$field_settings['dynamic']['param_name']] ) ) : $field_settings['meta_value'];
+        if ( isset( $field_settings['dynamic']['param_name'] ) ) {
+            if ( isset( $_GET[ $field_settings['dynamic']['param_name'] ] ) ) {
+                $value = $_GET[$field_settings['dynamic']['param_name']];
+            }
+        } else {
+            $value = $field_settings['meta_value'];
+        }
         ?>
         <input type="hidden" name="<?php echo esc_attr( $field_settings['name'] ); ?>" value="<?php echo esc_attr( $value ); ?>">
         <?php
