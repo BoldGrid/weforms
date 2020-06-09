cbb630b26a9aea03bccc30c65b2590efce519e1d
joe9663 <joe9663@gmail.com>
2020-06-09 12:03:31 -0400

    adds premium upsell cta to all forms page

--- assets/js-templates/spa-components.php
+++ assets/js-templates/spa-components.php
@@ -779,7 +779,12 @@
 <div class="contact-form-list">
     <h1 class="wp-heading-inline"><?php _e( 'All Forms', 'weforms' ); ?></h1>
     <a class="page-title-action add-form" herf="#" v-on:click.prevent="displayModal()"><?php _e( 'Add Form', 'weforms' ); ?></a>
-
+    <?php if ( ! class_exists( 'WeForms_Pro' ) ) {?>
+        <div>
+            <h2>You're using weForms Free. For more features, modules and more consider upgrading to Pro.</h2>
+            <a href="weformspro.com/pricing[utm]" target="_blank">UPGRADE NOW!</a>
+        </div>
+    <?php } ?>
     <wpuf-template-modal :show.sync="showTemplateModal" :onClose="closeModal"></wpuf-template-modal>
 
     <form-list-table></form-list-table>
