d700344cd91ecc2b2bdf7cff68a42b210752882d
joe9663 <joe9663@gmail.com>
2020-06-24 11:38:27 -0400

    adds translation support for privacy page text

--- assets/js-templates/spa-components.php
+++ assets/js-templates/spa-components.php
@@ -1086,28 +1086,34 @@
 <script type="text/x-template" id="tmpl-wpuf-weforms-page-privacy">
 <div class="weforms-privacy-page">
 
-<h1>Privacy</h1>
+<h1><?php _e('Privacy' , 'weforms'); ?></h1>
 
-<h2>Your website may need a Privacy Policy by law</h2>
+<h2><?php _e( 'Your website may need a Privacy Policy by law' , 'weforms' ); ?></h2>
 
-<p>We at WeForms take privacy law compliance seriously, and we want our customers to know just how important it is for them as well.<br>
-<br>
-Because you are implementing a contact form, that means you may be collecting the Personally Identifiable Information (PII) of individuals. This means that there may be privacy laws that apply to your website that require you to have a Privacy Policy. It is important to understand that if your website collects PII from users outside of your jurisdiction, you still may need to comply with the privacy laws of other states and countries. Please note that non-compliance may put you in danger of significant privacy related fines and lawsuits.<br>
-<br>
-If you do not have a Privacy Policy or are unsure if yours is up to date and compliant, we encourage you to generate one with our partner, Termageddon. Termageddon is a generator of Privacy Policies, Terms &amp; Conditions and more. They monitor privacy laws for you and update your Privacy Policy when new disclosures are required.<br>
-<br>
-If you decide Termageddon is a good solution for your website, use the promo code <strong>WEFORMS</strong> for 10&#37; off your first purchase at checkout. More information on Termageddon can be found <a href="https://app.termageddon.com/?fp_ref=weforms" target="_blank">here</a>.<br>
-    <br>
-If you own a web design, web development or digital marketing company, check out <a href="https://termageddon.com/agency-partners/" target="_blank">Termageddon's agency partner program</a>, where you can offer Termageddon licenses to your clients, helping them stay compliant with privacy laws.</p>
+<p><?php _e( 'We at weForms take privacy law compliance seriously, and we want our customers to know just how important it is for them as well.', 'weforms' ); ?></p>
 
-<h2>How to add Privacy Policy consent to your forms</h2>
+<p><?php _e( 'Because you are implementing a contact form, that means you may be collecting the Personally Identifiable Information (PII) of individuals. This means that there may be privacy laws that apply to your website that require you to have a Privacy Policy. It is important to understand that if your website collects PII from users outside of your jurisdiction, you still may need to comply with the privacy laws of other states and countries. Please note that non-compliance may put you in danger of significant privacy related fines and lawsuits.' , 'weforms' ); ?></p>
 
-<p>Adding a Privacy Policy consent checkbox is your forms is simple. All you will need is the WeForms plugin and an existing /privacy-policy page.</p>
+<p><?php _e( 'If you do not have a Privacy Policy or are unsure if yours is up to date and compliant, we encourage you to generate one with our partner, Termageddon. Termageddon is a generator of Privacy Policies, Terms &amp; Conditions and more. They monitor privacy laws for you and update your Privacy Policy when new disclosures are required.' , 'weforms' ); ?></p>
+
+<p><?php printf(
+        _e( 'If you decide Termageddon is a good solution for your website, use the promo code <strong>WEFORMS</strong> for 10&#37; off your first purchase at checkout. More information on Termageddon can be found <a href="%s" target="_blank">here</a>.' , 'weforms' ),
+        esc_url( 'https://app.termageddon.com/?fp_ref=weforms' ) 
+    ); ?></p>
+
+<p><?php printf(
+        _e( 'If you own a web design, web development or digital marketing company, check out <a href="%s" target="_blank">Termageddon&quot;s agency partner program</a>, where you can offer Termageddon licenses to your clients, helping them stay compliant with privacy laws.' , 'weforms' ),
+        esc_url( 'https://termageddon.com/agency-partners/' )
+    ); ?></p>
+
+<h2><?php _e( 'How to add Privacy Policy consent to your forms' , 'weforms' ); ?></h2>
+
+<p><?php _e( 'Adding a Privacy Policy consent checkbox is your forms is simple. All you will need is the WeForms plugin and an existing /privacy-policy page.' , 'weforms' ); ?></p>
 
 <ol>
-    <li>"Edit" the respective form that you'd like to add a consent checkbox to.</li>
-    <li>Click 'checkbox' so that it is added to the bottom of your form</li>
-    <li>"Edit" the consent checkbox, remove the 'Field Label', and replace 'Option' with 'INSERT YOUR "I AGREE" LANGUAGE HERE AND PROVIDE A LINK TO YOUR PRIVACY PPOLICY'.</li>
+    <li><?php _e( '"Edit" the respective form that you&quot;d like to add a consent checkbox to.' , 'weforms' ); ?></li>
+    <li><?php _e( 'Click &quot;checkbox&quot; so that it is added to the bottom of your form.' , 'weforms' ); ?></li>
+    <li><?php _e( '"Edit" the consent checkbox, remove the &quot;Field Label&quot;, and replace &quot;Option&quot; with &quot;INSERT YOUR "I AGREE" LANGUAGE HERE AND PROVIDE A LINK TO YOUR PRIVACY PPOLICY&quot;.' , 'weforms' ); ?></li>
 </ol>
 
 </div>
