56f87ba52f75a0d08348a8f538253f140d8a886b
maheshrw <maheshrw@users.noreply.github.com>
2020-10-13 17:41:56 +0530

    1.5.2

--- weforms.php
+++ weforms.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://weformspro.com/
  * Author: weForms
  * Author URI: https://weformspro.com/
- * Version: 1.5.1
+ * Version: 1.5.2
  * License: GPL2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: weforms
