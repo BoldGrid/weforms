dd5f85e0e468f6e105483bba7ce3e232cf31f114
Austin Von Ville <agvonville@gmail.com>
2021-05-24 15:56:45 -0400

    Added @todo tag

--- assets/wpuf/js/frontend-form.js
+++ assets/wpuf/js/frontend-form.js
@@ -10,8 +10,6 @@
             });
         });
     };
-    // Adding custom jQuery serialize wrapper to the frontend and backend for WeForms 1.6.7. 
-    // GitHub ticket created to make one script applied to frontend/backend.
     $.fn.extend({
         /**
         * Custom jQuery serialize wrapper.
@@ -19,7 +17,10 @@
         * When WordPress 5.6 increased the jQuery version to 3.5.1, the serialize function changed. Instead of
         * sending spaces as "+", they are sent as "%20". This wrapper is for backwards compatibility.
         *
-        * @since ###
+        * @todo This function is duplicated in both the frontend and backend. Need to have the code live in
+        * just one location.
+        *
+        * @since 1.6.7
         */
         weSerialize: function() {
             return $( this ).serialize().replaceAll( '%20', '+' );
